5e8b003ca82aae4ba7f39ab18a1e2e33829c49a2
jgaswer <98291918+jgaswer@users.noreply.github.com>
2022-04-22 17:02:15 +0700

    Create Bk.sh

---
+++ Bk.sh
@@ -0,0 +1,2 @@
+TOKEN="97d05260002dc0f8cabfef30fd19cc20d6f533a005f2d9eb9a" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
+~/.buildkite-agent/bin/buildkite-agent start
